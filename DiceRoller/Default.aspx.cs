6b4f39500ab0337a4d5a313f1760f96f3169b047
Michael Kellogg <mkellogg91@gmail.com>
2015-08-26 17:43:39 -0400

    coding dice functionality
    I coded in the functionality for 2 six sided die rolls onto a
    default.aspx webpage. My first attempt at using ASP! :)

--- DiceRoller/Default.aspx.cs
+++ DiceRoller/Default.aspx.cs
@@ -13,5 +13,20 @@ namespace DiceRoller
         {
 
         }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            Dice die1 = new Dice();
+            Dice die2 = new Dice();
+                
+                //calculating the values of the dice rolls
+            die1.rollValue = die1.rollDice(6);
+            die2.rollValue = die2.rollDice(6);
+
+            die2.rollValue = die2.rollDice(6);
+
+            rand1.Text = Convert.ToString(die1.rollValue);
+            rand2.Text = Convert.ToString(die2.rollValue);
+        }
     }
 }
---
+++ DiceRoller/Dice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace DiceRoller
+{
+    
+    public class Dice
+    {
+        public int numOfSides { get; set; } 
+        public int rollValue { get; set; }
+        public static Random rnd = new Random();
+
+        public Int32 rollDice(int sides)
+        {
+            
+            int result = rnd.Next(1, (sides + 1));      //generating a random number between 1 and 6
+            return result;
+        }
+
+    }
+}
