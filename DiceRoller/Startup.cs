51b0b499695d22d64513da64f61f64854ef852e2
Michael Kellogg <mkellogg91@gmail.com>
2015-08-26 00:32:25 -0400

    initial commit of vs files

---
+++ DiceRoller/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DiceRoller.Startup))]
+namespace DiceRoller
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
